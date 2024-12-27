b687ad1914cc103e209165bc2eec13c7dd5568c1
nikit <nikit@LAPTOP-C22L3FQB>
2024-12-27 09:23:08 +0300

    Основа

--- Tournament_421_KogorevNikitaEvgenievich/Tournament_421_KogorevNikitaEvgenievich/obj/Debug/App.g.i.cs
+++ Tournament_421_KogorevNikitaEvgenievich/Tournament_421_KogorevNikitaEvgenievich/obj/Debug/App.g.i.cs
@@ -41,20 +41,6 @@ namespace Tournament_421_KogorevNikitaEvgenievich {
     public partial class App : System.Windows.Application {
         
         /// <summary>
-        /// InitializeComponent
-        /// </summary>
-        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
-        public void InitializeComponent() {
-            
-            #line 5 "..\..\App.xaml"
-            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
-            
-            #line default
-            #line hidden
-        }
-        
-        /// <summary>
         /// Application Entry Point.
         /// </summary>
         [System.STAThreadAttribute()]
@@ -62,7 +48,6 @@ namespace Tournament_421_KogorevNikitaEvgenievich {
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
         public static void Main() {
             Tournament_421_KogorevNikitaEvgenievich.App app = new Tournament_421_KogorevNikitaEvgenievich.App();
-            app.InitializeComponent();
             app.Run();
         }
     }
